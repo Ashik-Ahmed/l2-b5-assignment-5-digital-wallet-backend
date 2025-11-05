2a2eb7b7ececfd1696a0e3b88a74e99e05bb7501
ashik-ahmed <ashikahmed121@gmail.com>
2025-11-05 17:16:37 +0600

    access token secure made based on node env

--- dist/app.js
+++ dist/app.js
@@ -15,7 +15,7 @@ app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 // app.use(cors());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000", // exact origin — not '*'
+    origin: ["http://localhost:3000", "https://l2-b5-assignment-6-digital-wallet-f.vercel.app"], // exact origin — not '*'
     credentials: true, // allow cookies/credentials
 }));
 app.use("/api/v1", routes_1.default);
